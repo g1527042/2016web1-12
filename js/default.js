7459a184bd4c9b6c7c39633befa34ab884d2614d
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-06-27 17:58:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -25,7 +25,7 @@ for(var i = 1; i < 10; i++){
   document.getElementById('price').appendChild(li);
 }
 
-var constellatio = {'牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'};
+var constellatio = ['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];
 for(var i = 0; i < 12; i++){
   var li = document.createElement('li');
   li.textContent = constellatio[i];
