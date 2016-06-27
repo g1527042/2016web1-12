483ad69272e7b687823351b1752b7d75b6b4c649
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-06-27 18:43:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -48,9 +48,11 @@ for(var i=1; i<=100; i++){
   var li = document.createElement('li');
   var Fizz = (i % 3 === 0) ? 'Fizz' : '';
   var Buzz = (i % 5 === 0) ? 'Buzz' : '';
-  if(i % 3 === 0 && i % 5 === 0){
+  if(i % 3 === 0 || i % 5 === 0){
     li.textContent = Fizz + Buzz ;
   }
-  li.textContent = i;
+  else{
+   li.textContent = i; 
+  }
   document.getElementById('quiz').appendChild(li);
 }
